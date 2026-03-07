65ad8737f5fa15569b5723d281be72b9503a98d0
AlecMLewis <aleclewis95@gmail.com>
2026-03-07 14:04:34 -0800

    Changed js for loading map to Google default

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-var map;
-function initMap() {
-  map = new google.maps.map(document.getElementById("map"), {
-    center: {lat: -34, lng: 150},
-    zoom: 8
-  });
+async function initMap() {
+    //  Request the needed libraries.
+    const [{ Map }, { AdvancedMarkerElement }] = await Promise.all([
+        google.maps.importLibrary('maps'),
+        google.maps.importLibrary('marker'),
+    ]);
+    // Get the gmp-map element.
+    const mapElement = document.querySelector('gmp-map');
+    // Get the inner map.
+    const innerMap = mapElement.innerMap;
+    // Set map options.
+    innerMap.setOptions({
+        mapTypeControl: false,
+    });
+    // Add a marker positioned at the map center (Uluru).
+    const marker = new AdvancedMarkerElement({
+        map: innerMap,
+        position: mapElement.center,
+        title: 'Uluru/Ayers Rock',
+    });
 }
+initMap();
