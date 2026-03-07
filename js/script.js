a5ae1b95f1cdf765d915fa5228b298361049eb5f
AlecMLewis <aleclewis95@gmail.com>
2026-03-07 15:32:15 -0800

    Testing new map setup

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-const mapElement = document.querySelector('gmp-map');
-async function initMap() {
-    const { Map } = (await google.maps.importLibrary('maps'));
-    const { AdvancedMarkerElement } = (await google.maps.importLibrary('marker'));
-    const marker = new AdvancedMarkerElement({
-        position: { lat: 47.24582, lng: -122.43405},
-        title: {"Tacoma Glassblowing Studio"},
+function initMap() {
+  
+    const markers = {
+      {
+        locationName: 'Museum of Glass',
+        lat: 47.245544471307014,
+        lng: -122.43401900264904,
+        address: '1801 Dock St, Tacoma, WA 98402'
+      }
+    }
+    
+    const mapOptions = {
+      center: lat: 47.245544471307014, lng: -122.43401900264904,
+      zoom: 10
+      disableDefaultUI: true
+    }
+    
+    const map = new google.maps.Map(document.getElementByID('map'), mapOptions);
     });
     mapElement.append(marker);
 }
