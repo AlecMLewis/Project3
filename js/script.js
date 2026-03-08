c3588ccbaf3bb5de096beb12762117f96bc61f5b
AlecMLewis <aleclewis95@gmail.com>
2026-03-07 17:46:25 -0800

    Changed map structure and adding more markers

--- js/script.js
+++ js/script.js
@@ -1,22 +1,55 @@
-function initMap() {
-  
+async function initMap() {
+    //  Request the needed libraries.
+    const [{ Map }, { AdvancedMarkerElement }] = await Promise.all([
+        google.maps.importLibrary('maps'),
+        google.maps.importLibrary('marker'),
+    ]);
+
+    const mapElement = document.querySelector('gmp-map');
+
+    const innerMap = mapElement.innerMap;
+
+    innerMap.setOptions({
+      mapTypeControl: false,
+    });
+    // Add a marker positioned at the map center (Uluru).
+    const marker = new AdvancedMarkerElement({
+      map: innerMap,
+      position: mapElement.center,
+      title: 'Tacoma',
+    });
+    
     const markers = {
       {
         locationName: 'Museum of Glass',
-        lat: 47.245544471307014,
+        lat: 47.24555175466093,
         lng: -122.43401900264904,
-        address: '1801 Dock St, Tacoma, WA 98402'
-      }
+        address: '1801 Dock St, Tacoma, WA 98402',
+      },
+      {
+        locationName: "Devil's Reef",
+        lat: 47.25739285397731,
+        lng: -122.44187783747694,
+        address: '706 Court C, Tacoma, WA 98402',
+      },
+      {
+        locationName: 'Top of Tacoma',
+        lat: 47.22933137026923,
+        lng: -122.42032177381394,
+        address: '3529 McKinley Ave, Tacoma, WA 98404',
+      },
+      {
+        locationName: 'Tacoma Public Library South Tacoma Branch',
+        lat: 47.20685819133857,
+        lng: -122.48184392442506,
+        address: '3411 S 56th St, Tacoma, WA 98409',
+      },
+      {
+        locationName: 'Wapato Hills Park',
+        lat: 47.200209588678284,
+        lng: -122.46805894894034,
+        address: '6231 S Wapato St, Tacoma, WA 98409',
+      },
     }
-    
-    const mapOptions = {
-      center: lat: 47.245544471307014, lng: -122.43401900264904,
-      zoom: 10
-      disableDefaultUI: true
-    }
-    
-    const map = new google.maps.Map(document.getElementByID('map'), mapOptions);
-    });
-    mapElement.append(marker);
 }
 initMap();
