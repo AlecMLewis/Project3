f933ec42827750ddf6270740b92893a203becdb6
AlecMLewis <aleclewis95@gmail.com>
2026-03-07 18:39:06 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,41 +42,5 @@ async function initMap() {
       position: { lat: 47.200209588678284, lng: -122.46805894894034 },
       title: "Wapato Hills Park",
     });
-    
-    /*
-    const markers = {
-      {
-        map: innerMap
-        locationName: 'Museum of Glass',
-        lat: 47.24555175466093,
-        lng: -122.43401900264904,
-        address: '1801 Dock St, Tacoma, WA 98402',
-      },
-      {
-        locationName: "Devil's Reef",
-        lat: 47.25739285397731,
-        lng: -122.44187783747694,
-        address: '706 Court C, Tacoma, WA 98402',
-      },
-      {
-        locationName: 'Top of Tacoma',
-        lat: 47.22933137026923,
-        lng: -122.42032177381394,
-        address: '3529 McKinley Ave, Tacoma, WA 98404',
-      },
-      {
-        locationName: 'Tacoma Public Library South Tacoma Branch',
-        lat: 47.20685819133857,
-        lng: -122.48184392442506,
-        address: '3411 S 56th St, Tacoma, WA 98409',
-      },
-      {
-        locationName: 'Wapato Hills Park',
-        lat: 47.200209588678284,
-        lng: -122.46805894894034,
-        address: '6231 S Wapato St, Tacoma, WA 98409',
-      },
-    }
-    */
 }
 initMap();
